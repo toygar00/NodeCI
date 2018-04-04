a49853284be05f8f04114661df9a4081b33aaa2c
Toygar Onbay <judastom@gmail.com>
2018-04-04 14:01:43 +0200

    first

---
+++ config/dev.js
@@ -0,0 +1,7 @@
+module.exports = {
+  googleClientID:
+    '964808011168-29vqsooppd769hk90kjbjm5gld0glssb.apps.googleusercontent.com',
+  googleClientSecret: 'KnH-rZC23z4fr2CN4ISK4srN',
+  mongoURI: 'mongodb://toygar2:123123@ds247357.mlab.com:47357/blog_dev',
+  cookieKey: '123123123'
+};
